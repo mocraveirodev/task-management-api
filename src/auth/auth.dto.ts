020a5ddc84c6a9a7a6fd40bf2f6b1884bb86ae79
Monica Craveiro <craveiromonica@gmail.com>
2024-08-25 20:50:16 -0300

    Add validation

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,4 +1,9 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class AuthResponseDto {
+  @IsString()
   token: string;
+
+  @IsNumber()
   expiresIn: number;
 }
--- src/users/user.dto.ts
+++ src/users/user.dto.ts
@@ -1,5 +1,23 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class UserDto {
+  @IsUUID()
+  @IsOptional()
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
